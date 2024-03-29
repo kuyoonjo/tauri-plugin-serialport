397544f57d8f10b7da7b94a52d25147ef7511c1b
lzhida <zhidal@qq.com>
2024-03-29 10:20:24 +0800

    test: 🧪 修改测试用例内容

--- examples/svelte-app/src/App.svelte
+++ examples/svelte-app/src/App.svelte
@@ -5,6 +5,8 @@
 
   let path = 'COM18';
   let baudRate = 9600;
+  let message = '';
+  let reaveiveData = '';
 
   function openSerialport() {
     serialport = new Serialport({ path: path, baudRate: baudRate });
@@ -41,7 +43,7 @@
 
   function write() {
     serialport
-      .write('1234')
+      .write(message)
       .then((res) => {
         console.log('write serialport: ', res);
       })
@@ -85,6 +87,22 @@
       });
   }
 
+  function listenStr() {
+    const decoder = new TextDecoder('utf-8');
+    serialport
+      .listen((data: any) => {
+        console.log('listen serialport data: ', data);
+        const str = decoder.decode(data);
+        reaveiveData = reaveiveData + str + '\n';
+      }, false)
+      .then((res) => {
+        console.log('listen serialport: ', res);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
   function cancelRead() {
     serialport
       .cancelRead()
@@ -118,14 +136,32 @@
 </script>
 
 <div>
-  <button on:click={available_ports}>Available Ports</button>
-  <button on:click={openSerialport}>Open</button>
-  <button on:click={close}>Close</button>
-  <button on:click={write}>Write</button>
-  <button on:click={writeBinary}>writeBinary</button>
-  <button on:click={read}>Read</button>
-  <button on:click={listen}>listen</button>
-  <button on:click={cancelRead}>cancelRead</button>
-  <button on:click={closeAll}>closeAll</button>
-  <button on:click={forceClose}>forceClose</button>
+  <div>
+    <button on:click={available_ports}>Available Ports</button>
+    <button on:click={openSerialport}>Open</button>
+    <button on:click={close}>Close</button>
+    <button on:click={write}>Write</button>
+    <button on:click={writeBinary}>writeBinary</button>
+    <button on:click={read}>Read</button>
+    <button on:click={listen}>listen</button>
+    <button on:click={listenStr}>listenStr</button>
+    <button on:click={cancelRead}>cancelRead</button>
+    <button on:click={closeAll}>closeAll</button>
+    <button on:click={forceClose}>forceClose</button>
+  </div>
+  <div>
+    <input
+      type="text"
+      bind:value={path}
+    />
+    <input
+      type="number"
+      bind:value={baudRate}
+    />
+  </div>
+
+  <div>
+    <textarea bind:value={message} />
+    <textarea bind:value={reaveiveData} />
+  </div>
 </div>
